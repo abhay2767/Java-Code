2f62dabd84cf0e7f23a918ede64be48b76ed59ec
Abhay <abhaydubey2767@gmail.com>
2023-08-24 11:11:05 +0530

    Initial commit

---
+++ first.java
@@ -0,0 +1,16 @@
+class c1
+{
+    public void m1()
+    {
+        System.out.println("m1 of c1");
+   
+    }
+}
+public class first
+{
+    public static void main(String args[])
+    {
+        c1 obj = new c1();
+        obj.m1();
+    }
+}
